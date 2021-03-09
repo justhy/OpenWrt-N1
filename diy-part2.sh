8e7b7b81015a343d60fb386f4eab5337c589eca4
Godcic <Godcic@foxmail.com>
2021-03-09 23:52:46 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,7 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.6.1/g' package/base-files/files/bin/config_generate
-
-# lean等源码编译bypass前请先执行
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
-find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
